3229568d6426ba3a9b87e367cc11782fd571abba
loparquo <84879546+loparquo@users.noreply.github.com>
2021-05-27 04:04:17 +0600

    my first inputing form

---
+++ script.js
@@ -0,0 +1,45 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+
+//second function
+
+   function digitalClock() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+       //* добавление ведущих нулей */
+      if (hours < 10) hours = "0" + hours;
+      if (minutes < 10) minutes = "0" + minutes;
+      if (seconds < 10) seconds = "0" + seconds;
+        document.getElementById("id_clock").innerHTML = hours + ":" + minutes;
+        setTimeout("digitalClock()", 1000);
+   }
+
+   //*кнопки*/
+   butt.onclick = function() {
+    var val = document.getElementById('elem1').value;
+    document.getElementById('str').innerHTML=val;
+    var val = document.getElementById('elem2').value;
+        document.getElementById('str1').innerHTML=val;
+    };
